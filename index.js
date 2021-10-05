7700288f0cbe49e8d8c3fccef1ab9803503199cb
Heath Griffith <heathlgriffith@gmail.com>
2021-10-05 12:36:56 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(pizazz = "*") {
+    return function(adjective = "special") {
+        return `You are ${pizazz}${adjective}${pizazz}!`;
+    }
+}
